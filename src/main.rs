eeda626df2cdbc1a7d4603b9c5f77a266781cab1
Morten Lohne <lohnemorten@gmail.com>
2019-07-16 16:11:06 +0200

    Clippy adjustments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate rand;
 extern crate shakmaty;
 extern crate shakmaty_syzygy;
 
-use shakmaty::{Board, Chess, Color, FromSetup, Piece, Position, Role, fen};
+use shakmaty::{fen, Board, Chess, Color, FromSetup, Piece, Position, Role};
 use shakmaty_syzygy::{Dtz, Tablebase, Wdl};
 
 use rand::Rng;
@@ -26,41 +26,38 @@ fn main() {
     iter::repeat_with(|| generate_6man_with_eval(&tables, &mut rng))
         .filter(|(_, _, dtz)| dtz.0.abs() > 10 && dtz.0.abs() < 100)
         .filter_map(|(pos, wdl, dtz)| {
-            let mut children = pos.legals()
+            let mut children = pos
+                .legals()
                 .iter()
                 .map(|mv| (mv.clone(), pos.clone().play(mv).unwrap()))
                 .map(|(mv, child_pos)| (mv, tables.probe_dtz(&child_pos).unwrap()))
                 .filter(|(_, child_dtz)| (child_dtz.0 + dtz.0).abs() <= 4)
                 .collect::<Vec<_>>();
 
-            children.sort_by_key(|(_, child_dtz)| child_dtz.clone());
+            children.sort_by_key(|(_, child_dtz)| child_dtz.0);
             children.reverse();
 
             debug_assert!(!children.is_empty(), "{}: {:?}", fen::fen(&pos), dtz);
             if children.len() > 1 && (children[1].1 + dtz).0.abs() > 2 {
                 Some((pos, wdl, dtz, children[0].0.clone()))
-            }
-            else {
+            } else {
                 None
             }
         })
-        .for_each(|(pos, wdl, dtz, mv)| {
-            match wdl  {
-                Wdl::Win => println!(
-                        "{} hmvc {}; bm {}",
-                        fen::epd(&pos), 100 - dtz.0, mv
-                    ),
-                Wdl::Loss => println!(
-                    "{} hmvc {}; bm {}",
-                    fen::epd(&pos), 100 - dtz.0.abs() + 1, mv
-                ),
-                _ => unreachable!(),
-            }
+        .for_each(|(pos, wdl, dtz, mv)| match wdl {
+            Wdl::Win => println!("{} hmvc {}; bm {}", fen::epd(&pos), 100 - dtz.0, mv),
+            Wdl::Loss => println!(
+                "{} hmvc {}; bm {}",
+                fen::epd(&pos),
+                100 - dtz.0.abs() + 1,
+                mv
+            ),
+            _ => unreachable!(),
         });
 }
 
 fn generate_6man_with_eval<R: Rng>(tables: &Tablebase<Chess>, rng: &mut R) -> (Chess, Wdl, Dtz) {
-    let pos: Chess = generate_random_6man(rng).into();
+    let pos: Chess = generate_random_6man(rng);
 
     let wdl = tables.probe_wdl(&pos).unwrap();
 
@@ -105,9 +102,10 @@ fn generate_random_6man<R: Rng>(rng: &mut R) -> Chess {
             4 => Role::Queen,
             _ => unreachable!(),
         };
-        let color = match rng.gen_bool(0.5) {
-            true => Color::White,
-            false => Color::Black,
+        let color = if rng.gen() {
+            Color::White
+        } else {
+            Color::Black
         };
         loop {
             let square = shakmaty::Square::new(rng.gen_range(0, 64));
@@ -119,9 +117,10 @@ fn generate_random_6man<R: Rng>(rng: &mut R) -> Chess {
         }
     }
 
-    let color = match rng.gen_bool(0.5) {
-        true => Color::White,
-        false => Color::Black,
+    let color = if rng.gen() {
+        Color::White
+    } else {
+        Color::Black
     };
 
     let fen = shakmaty::fen::Fen {
